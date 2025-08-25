7b0fae648f3e2da37bc9b4d77a9eb528c314cc9c
Prithiv G <prithiv.g@laderatechnology.com>
2025-08-25 09:50:30 +0000

    recent commit

--- webapp/controller/Cart.controller.js
+++ webapp/controller/Cart.controller.js
@@ -6,7 +6,35 @@ sap.ui.define([
 
     return Controller.extend("decathlon.controller.Cart", {
         onInit() {
-            
         },
+             onIncreaseQuantity: function (oEvent) {
+            var oBindingvalue = oEvent.getSource().getBindingContext("cart");
+            var oValue = oBindingvalue.getObject();
+            oValue.quantity++;
+            oBindingvalue.getModel().refresh(true);
+        },
+
+        onDecreaseQuantity: function (oEvent) {
+            var oBindingvalue = oEvent.getSource().getBindingContext("cart");
+            var oValue = oBindingvalue.getObject();
+            if (oValue.quantity > 1) {
+                oValue.quantity--;
+            } else {
+                MessageToast.show("Minimum 1 quantity is required...");
+            }
+            oBindingvalue.getModel().refresh(true);
+        },
+
+        onRemoveFromCart: function (oEvent) {
+            var oBindingvalue = oEvent.getSource().getBindingContext("cart");
+            var oModel = oBindingvalue.getModel();
+            var aItems = oModel.getProperty("/items");
+
+            var iIndex = oBindingvalue.getPath().split("/").pop();
+            aItems.splice(iIndex, 1);
+
+            oModel.setProperty("/items", aItems);
+            MessageToast.show("Item removed from cart");
+        }
     });
 });
--- webapp/controller/ShoesPage.controller.js
+++ webapp/controller/ShoesPage.controller.js
@@ -6,69 +6,40 @@ sap.ui.define([
 
     return Controller.extend("decathlon.controller.ShoesPage", {
         onInit() {
-            var oModel = new sap.ui.model.json.JSONModel();
-            oModel.loadData("model/cart.json"); 
-            this.getView().setModel(oModel, "products");
+            var oProductsModel = new sap.ui.model.json.JSONModel();
+            oProductsModel.loadData("model/cart.json");
+            this.getView().setModel(oProductsModel, "products");
 
-            
-            var oCartModel = new sap.ui.model.json.JSONModel({
-                items: []
-            });
-            this.getView().setModel(oCartModel, "cart");
-
-            var oModel1 = this.getOwnerComponent().getModel("cart");
-            var oCartValue = oModel1.getProperty("/items");
-          
+            var oCartModel = this.getOwnerComponent().getModel("cart");
+            if (!oCartModel) {
+                oCartModel = new sap.ui.model.json.JSONModel({ items: [] });
+                this.getOwnerComponent().setModel(oCartModel, "cart");
+            }
+    this.getView().setModel(oCartModel, "cart");
         },
          onPressWishlist:function(oEvent){
             var oButton = oEvent.getSource();
             var oCurrentIcon = oButton.getIcon();
 
             if (oCurrentIcon === "sap-icon://heart-2") {
-                oButton.setIcon("sap-icon://heart");    
+                oButton.setIcon("sap-icon://heart"); 
+                 MessageToast.show("Added to Wishlist");   
             }else {
                 oButton.setIcon("sap-icon://heart-2");
+                 MessageToast.show("Removed from Wishlist");
             }
-            MessageToast.show("Added to Wishlist");
         },
         onPressAddToCart:function(oEvent){
-             var oVBox = oEvent.getSource().getParent(); 
+            var oView = this.getView();
+            var oCartModel = oView.getModel("cart");
+            var aCartItems = oCartModel.getProperty("/items");
 
-            var sName = oVBox.getItems()[2].getText();          // Product name
-            var sDesc = oVBox.getItems()[3].getText();          // Description
-            var sPrice = oVBox.getItems()[4].getItems()[0].getNumber(); // Price
-            var sCurrency = oVBox.getItems()[4].getItems()[0].getUnit();
-
-            // Access cart model
-            // var oCartModel = this.getView().getModel("cart");
-            // var aCartItems = oCartModel.getProperty("/items");
-
-            // Check if product already in cart
-            // oCartValue.push({
-            //         name: sName,
-            //         description: sDesc,
-            //         price: parseFloat(sPrice),
-            //         currency: sCurrency,
-            //         quantity: 1
-            //     });
-
-            // var oExisting = oCartValue.find(item => item.name === sName);
-
-            // if (oExisting) {
-            //     oExisting.quantity += 1; // Increase quantity
-            // } else {
-            //     aCartItems.push({
-            //         name: sName,
-            //         description: sDesc,
-            //         price: parseFloat(sPrice),
-            //         currency: sCurrency,
-            //         quantity: 1
-            //     });
-            // }
-
-            // Update cart model
-            oCartValue.setProperty("/items", oCartValue);
-
+            var oButton = oEvent.getSource();
+            var oProductContext = oButton.getBindingContext("products");
+            var oProductData = oProductContext.getObject();
+            oProductData.Quantity = 1;
+            oCartModel.setProperty("/items", [oProductData]);
+            oCartModel.refresh(true);
             MessageToast.show("Added to your Cart...!!")
         },
     });
