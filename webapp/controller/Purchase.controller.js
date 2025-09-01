5c51446a241aad2fd88a71668a704be4043b5ec8
Prithiv G <prithiv.g@laderatechnology.com>
2025-09-01 10:15:45 +0000

    recent commit

--- webapp/controller/Purchase.controller.js
+++ webapp/controller/Purchase.controller.js
@@ -6,7 +6,12 @@ sap.ui.define([
 
     return Controller.extend("decathlon.controller.Purchase", {
         onInit() {
-
-        }
+            var oProductsModel = new sap.ui.model.json.JSONModel();
+            oProductsModel.loadData("model/cart.json");
+            this.getView().setModel(oProductsModel, "products");
+            // var oModel = new sap.ui.model.json.JSONModel(sap.ui.require.toUrl("decathlon/webapp/model/cart.json"));
+            // this.getView().setModel(oModel, "products");
+        },
+        
     });
 });
