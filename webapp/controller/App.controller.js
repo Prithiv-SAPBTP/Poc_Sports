2a1e1db05e7fd0f9bf6a190600aa19751defcf5f
Prithiv G <prithiv.g@laderatechnology.com>
2025-09-02 12:27:17 +0000

    new commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,7 +4,27 @@ sap.ui.define([
   "use strict";
 
   return BaseController.extend("decathlon.controller.App", {
-      onInit() {
+          onInit: function () {
+      this.getOwnerComponent().getRouter().attachRouteMatched(this.onRouteMatched, this);
+    },
+ 
+    onRouteMatched: function (oEvent) {
+      var sRouteName = oEvent.getParameter("name");
+      var oPage = this.byId("appPage");
+
+      if (sRouteName == "RouteLoginPage") {
+        oPage.setVisible(false);
+      } else {
+        oPage.setVisible(true);
       }
+    },
+     onPressCart: function () {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                    oRouter.navTo("Cart")
+        },
+       onPressHelp: function(oEvent){
+        var oButton = oEvent.getSource();
+		this.byId("actionSheet").openBy(oButton);
+       }
   });
 });
